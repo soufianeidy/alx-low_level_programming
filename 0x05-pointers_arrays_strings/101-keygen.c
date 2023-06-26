828f339d8a313a21d3f6d31b43790a4ce7673b65
soufianeidy <soufianeidy1998@gmail.com>
2023-06-26 10:17:26 -0700

    my project number 6 C

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,38 +2,35 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define PASSWORD_LENGTH 12
-
-char *generate_password()
-{
-    static const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    char *password = malloc((PASSWORD_LENGTH + 1) * sizeof(char));
-    
-    if (password == NULL)
-    {
-        fprintf(stderr, "Memory allocation failed\n");
-        exit(1);
-    }
-
-    srand(time(NULL));
-
-    for (int i = 0; i < PASSWORD_LENGTH; i++)
-    {
-        int index = rand() % (sizeof(charset) - 1);
-        password[i] = charset[index];
-    }
-
-    password[PASSWORD_LENGTH] = '\0';
-
-    return password;
-}
-
+/**
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    char *password = generate_password();
-    printf("%s\n", password);
-    free(password);
+	int pass[100];
+	int i, sum, n;
 
-    return 0;
+	sum = 0;	
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
+
+	return (0);
 }
 
